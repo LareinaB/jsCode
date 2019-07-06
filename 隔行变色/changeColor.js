0f63fd283ecf3d7828f037f72dd1b51643bfec29
lubing <13546366192qq.com>
2019-07-06 14:31:30 +0800

    隔行变色

---
+++ 隔行变色/changeColor.js
@@ -0,0 +1,39 @@
+var newsBox = document.getElementById('newsBox'),
+    oList = newsBox.getElementsByTagName('li');
+
+// 跟选项卡一个原理
+for (var i = 0; i < oList.length; i++) {
+    // if(i % 2 === 1){
+    //     oList[i].className = 'c1';
+    // }else{
+    //     oList[i].className = 'c2';
+    // }
+    oList[i].myClassName = 'c' + (i % 2 + 1);
+    oList[i].className = oList[i].myClassName;
+    oList[i].onmouseover = function () {
+        // this 操作的li
+        this.className = 'hover';
+    };
+    oList[i].onmouseout = function () {
+        // this 操作的li
+        this.className = oList[i].myClassName;
+    }
+}
+
+// for (let i = 0; i < oList.length; i++) {
+//     // if(i % 2 === 1){
+//     //     oList[i].className = 'c1';
+//     // }else{
+//     //     oList[i].className = 'c2';
+//     // }
+//     let myClassName = 'c' + (i % 2 + 1);
+//     oList[i].className = myClassName;
+//     oList[i].onmouseover = function () {
+//         // this 操作的li
+//         this.className = 'hover';
+//     };
+//     oList[i].onmouseout = function () {
+//         // this 操作的li
+//         this.className = myClassName;
+//     }
+// }
