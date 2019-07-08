43307fdaf4ee04a2afd7ec43f74849bb0d5668b4
lubing <13546366192qq.com>
2019-07-08 11:21:56 +0800

    完善检测公有属性的方法

--- 原型和原型链/构造函数模式.js
+++ 原型和原型链/构造函数模式.js
@@ -1,20 +1,29 @@
 // var obj = {}; // 字面量方式
 // var obj = new Object(); // 实例创建 构造函数模式
 // 工厂模式
-function createUser(name, age) {
+function CreateUser(name, age) {
     this.name = name;
     this.age = age;
+    this.song = song;
+    this.sing = function () {
+        console.log(`my name is ${this.name}, i'm ${this.age} years old, i want to sing ${this.song}`);
+    }
 }
 // 实例的共有属性
-createUser.prototype.say = function () {
+CreateUser.prototype.say = function () {
     console.log(`hello javascript ~~, my name is ${this.name}, i'm ${this.age} years old`);
 };
 
-var user1 = createUser("lulu", 22);
-var user2 = createUser("pao", 22);
+var user1 = CreateUser("lulu", 22, "告白气球");
+var user2 = CreateUser("pao", 22, "是风动");
+console.log(user1.sing() === user2.sing()); // false
 console.log(user1.say() === user2.say());  // true
 
 // attr是否为obj的共有属性  自己实现的哈哈哈
-// function hasPubProperty(obj, attr) {
-//     return (attr in obj) && (!obj.hasOwnProperty(attr));
-// }
+Object.prototype.hasPubProperty = function () {
+    var attr = arguments[0];
+    if(typeof attr === 'undefined'){
+        return false;
+    }
+    return (attr in this) && (!this.hasOwnProperty(attr));
+};
